7711da70316e3f8396cb8113a00dc618137785b8
김진아 <c2010333@naver.com>
2023-09-20 17:56:48 +0900

    MainActivity Feature 추가

--- app/src/main/java/com/example/image_search/Adapter/ViewPager2Adapter.kt
+++ app/src/main/java/com/example/image_search/Adapter/ViewPager2Adapter.kt
@@ -3,14 +3,10 @@ package com.example.image_search.Adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.image_search.Data.SearchData
-import com.example.image_search.SearchFragment
 
 class ViewPager2Adapter(fragmentActivity: FragmentActivity) :
-        FragmentStateAdapter(fragmentActivity) {
-    private lateinit var viewPager2Adapter: ViewPager2Adapter
-    val fragments = listOf<SearchData>(SearchFragment())
-
+    FragmentStateAdapter(fragmentActivity) {
+    private val fragments : List<Fragment> = mutableListOf()
     override fun getItemCount(): Int {
         return fragments.size
     }
